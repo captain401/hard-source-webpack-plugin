f9e62ee9017afcb1623278a30f71a414878cb683
Tyler Arbus <me@tylerarbus.com>
2018-06-19 13:36:16 -0700

    Add String.padStart polyfill

--- index.js
+++ index.js
@@ -29,6 +29,29 @@ const SerializerJsonPlugin = require('./lib/SerializerJsonPlugin');
 
 const hardSourceVersion = require('./package.json').version;
 
+
+/*
+ * Add a String.prototype.padStart polyfill for node 6 compatability
+ * https://github.com/uxitten/polyfill/blob/master/string.polyfill.js
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
+ */
+if (!String.prototype.padStart) {
+  String.prototype.padStart = function padStart(targetLength,padString) {
+    targetLength = targetLength>>0; //truncate if number or convert non-number to 0;
+    padString = String((typeof padString !== 'undefined' ? padString : ' '));
+    if (this.length > targetLength) {
+      return String(this);
+    }
+    else {
+      targetLength = targetLength-this.length;
+      if (targetLength > padString.length) {
+        padString += padString.repeat(targetLength/padString.length); //append to original to ensure we are longer than needed
+      }
+      return padString.slice(0,targetLength) + String(this);
+    }
+  };
+}
+
 function requestHash(request) {
   return crypto
     .createHash('sha1')
